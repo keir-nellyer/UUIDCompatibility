f3477046fb1fd674b970843406f17c14e3d56071
Keir Nellyer <keir@nellyer.co.uk>
2014-06-29 15:17:27 +0100

    Retrieve UUIDs <-> Player Names from sources such as Essentials & Player DAT files Add warning users must accept before the plugin will take effect

--- src/main/java/com/ikeirnez/uuidcompatibility/UUIDCompatibility.java
+++ src/main/java/com/ikeirnez/uuidcompatibility/UUIDCompatibility.java
@@ -1,9 +1,14 @@
 package com.ikeirnez.uuidcompatibility;
 
+import net.ess3.api.IEssentials;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.UUID;
 
 /**
  * Created by iKeirNez on 29/06/2014.
@@ -18,7 +23,7 @@ public class UUIDCompatibility extends JavaPlugin {
         return instance;
     }
 
-    private CustomConfigWrapper nameMappingsWrapper;
+    private CustomConfigWrapper nameMappingsWrapper, retrievesWrapper;
 
     {
         instance = this;
@@ -26,9 +31,61 @@ public class UUIDCompatibility extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getDataFolder().mkdir();
+        saveDefaultConfig();
+
+        if (!getConfig().getBoolean("enabled")){
+            getLogger().severe("The plugin enabled status has not been set to true in the config, disabling...");
+            setEnabled(false);
+            return;
+        }
+
         nameMappingsWrapper = new CustomConfigWrapper(new File(getDataFolder(), "nameMappings.yml"));
+        retrievesWrapper = new CustomConfigWrapper(new File(getDataFolder(), "retrieves.yml"));
         getServer().getPluginManager().registerEvents(new LoginListener(), this);
+
+        if (!getRetrievesWrapper().getConfig().getBoolean("retrieved.world-data")){
+            getLogger().info("Retrieving UUID <-> Names from player dat files, please wait...");
+
+            for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()){
+                String uuidString = offlinePlayer.getUniqueId().toString();
+
+                if (!getNameMappingsWrapper().getConfig().contains(uuidString)){
+                    getNameMappingsWrapper().getConfig().set(uuidString, offlinePlayer.getName());
+                }
+            }
+
+            getNameMappingsWrapper().saveConfig();
+            getRetrievesWrapper().getConfig().set("retrieved.world-data", true);
+            getRetrievesWrapper().saveConfig();
+        }
+
+        if (!getRetrievesWrapper().getConfig().getBoolean("retrieved.essentials", false)){
+            Plugin essentialsPlugin = getServer().getPluginManager().getPlugin("Essentials");
+
+            if (essentialsPlugin != null){
+                getLogger().info("Retrieving UUID <-> Names from Essentials data, please wait...");
+                IEssentials essentials = (IEssentials) essentialsPlugin;
+
+                for (File file : new File(essentials.getDataFolder(), "userdata/").listFiles()){
+                    String fileName = file.getName();
+
+                    if (fileName.endsWith(".yml")){
+                        try {
+                            UUID uuid = UUID.fromString(fileName.substring(0, fileName.length() - 4));
+                            String uuidString = uuid.toString();
+
+                            if (!getNameMappingsWrapper().getConfig().contains(uuidString)){
+                                getNameMappingsWrapper().getConfig().set(uuidString, essentials.getUser(uuid).getLastAccountName());
+                            }
+                        } catch (IllegalArgumentException e){}
+                    }
+                }
+
+                getNameMappingsWrapper().saveConfig();
+                getRetrievesWrapper().getConfig().set("retrieved.essentials", true);
+                getRetrievesWrapper().saveConfig();
+            }
+        }
     }
 
     @Override
@@ -38,5 +95,9 @@ public class UUIDCompatibility extends JavaPlugin {
 
     public CustomConfigWrapper getNameMappingsWrapper() {
         return nameMappingsWrapper;
+    }
+
+    public CustomConfigWrapper getRetrievesWrapper() {
+        return retrievesWrapper;
     }
 }
